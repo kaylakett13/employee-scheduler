7e28e6e3fc3d103b74b00b0f55094f23e0cff7fe
KKETTERE <kkettere@ford.com>
2024-01-25 10:40:32 -0500

    Began setting up menu

--- employee-scheduler/src/app/app-routing.module.ts
+++ employee-scheduler/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component: HomeMenuComponent,
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- employee-scheduler/src/app/app.module.ts
+++ employee-scheduler/src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeMenuComponent } from './home-menu/home-menu.component';
+import { NavComponent } from './nav/nav.component';
+
+//PrimeNg Imports
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+
 
 @NgModule({
+  bootstrap: [AppComponent],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeMenuComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ButtonModule,
+    CardModule
   ],
   providers: [
     provideClientHydration()
   ],
-  bootstrap: [AppComponent]
 })
 export class AppModule { }
